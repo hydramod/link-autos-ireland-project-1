addb2837d404af3bb2dae40bec5923d26a74f229
Ali <alissaeid@live.co.uk>
2023-01-30 20:14:58 +0000

    Add Javascript for contatc form

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -11,3 +11,26 @@ button.addEventListener("click", function () {
   //Toggle the "collapse" class on the menu element
   menu.classList.toggle("collapse");
 });
+
+//**contact form**//
+
+// Get references to the form and success/error message elements
+const form = document.querySelector("form");
+const successMessage = document.querySelector(".alert-success");
+const errorMessage = document.querySelector(".alert-danger");
+
+// Add a submit event listener to the form
+form.addEventListener("submit", (event) => {
+  // Prevent the form from submitting
+  event.preventDefault();
+
+  // Show the success message
+  successMessage.removeAttribute("hidden");
+
+  // Set a timeout to hide the success message and reset the form
+  setTimeout(() => {
+    successMessage.setAttribute("hidden", true);
+    errorMessage.setAttribute("hidden", true);
+    form.reset();
+  }, 10000);
+});
